94d05b8e8d027c893cf3b30e43e6a06d542b2928
Jessica Scanlan <jessica.scanlan.1993@gmail.com>
2018-04-09 13:09:39 +1000

    made a stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+
+  get 'answer', to: 'questions#answer', as: :answer
+
+  root to: 'questions#ask'
+
+  post 'question', to: 'questions#answer'
+
+  # get 'questions/:answer', to: 'questions#answer', as: :answer
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
